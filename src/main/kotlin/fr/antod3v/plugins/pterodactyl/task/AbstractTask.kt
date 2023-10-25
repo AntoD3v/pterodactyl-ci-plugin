5d274c11345a160ed25849d3749ce8d7d53d3070
Antoine (AntoDev) <antoine@rivrs.io>
2023-10-25 23:36:20 +0200

    fix 8

--- src/main/kotlin/fr/antod3v/plugins/pterodactyl/task/AbstractTask.kt
+++ src/main/kotlin/fr/antod3v/plugins/pterodactyl/task/AbstractTask.kt
@@ -5,7 +5,7 @@ import com.mattmalec.pterodactyl4j.client.entities.ClientServer
 import fr.antod3v.plugins.pterodactyl.extension.Credential
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.options.Option
 
 abstract class AbstractTask : DefaultTask() {
@@ -13,14 +13,17 @@ abstract class AbstractTask : DefaultTask() {
     private val extension = project.extensions.findByName("pterodactyl") as Credential
 
     @Input
+    @Optional
     @Option(option = "serverId", description = "Id of the server (8 first characters of uuid)")
     var serverId: String? = extension.serverId
 
     @Input
+    @Optional
     @Option(option = "apiUrl", description = "Url of the pterodactyl api")
     var apiUrl: String? = extension.apiUrl
 
     @Input
+    @Optional
     @Option(option = "apiKey", description = "Key of the pterodactyl api")
     var apiKey: String? = extension.apiKey
 
