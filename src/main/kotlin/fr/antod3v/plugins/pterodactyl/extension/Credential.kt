c546450ffa607fdb5b7e4d1786b8b4dd98d7f397
Antoine (AntoDev) <antoine@rivrs.io>
2023-10-25 20:04:08 +0200

    fix var credential

--- src/main/kotlin/fr/antod3v/plugins/pterodactyl/extension/Credential.kt
+++ src/main/kotlin/fr/antod3v/plugins/pterodactyl/extension/Credential.kt
@@ -1,7 +1,7 @@
 package fr.antod3v.plugins.pterodactyl.extension
 
 data class Credential(
-    val apiUrl: String,
-    val apiKey: String,
-    val serverId: String? = null,
+    var apiUrl: String,
+    var apiKey: String,
+    var serverId: String? = null,
 )
